302f8d380090c0cbedfe64583332989d46ee8e6d
cekys <10346089+cekys@users.noreply.github.com>
2020-06-10 11:41:12 +0800

    update

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+let vm = new Vue(
+    {
+        el: ".container",
+        data: {
+            active: "home"
+        },
+        methods: {
+            navActive: function (item) {
+                this.active = item
+            },
+            rollToTop: function () {
+                window.scrollTo(
+                    {
+                        left: 0,
+                        top: 0,
+                        behavior: "smooth"
+                    }
+                ),
+                vm.navActive("home"),
+                alert(window.location.href)
+            }
+        }
+    }
+);
