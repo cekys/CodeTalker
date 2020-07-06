ad3439e035471180ea0228a93e42e88b4a3f9237
cekys <10346089+cekys@users.noreply.github.com>
2020-07-06 17:32:55 +0800

    update qr function

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@
 let vm = new Vue({
     el: ".container-fluid",
     data: {
-        active: "home"
+        active: "url"
     },
     methods: {
         navActive: function (item) {
             this.active = item
         }
     }
-});
+})
 
 // pure js
 function getQueryString(name) {
@@ -28,3 +28,30 @@ function rollToTop() {
         behavior: "smooth"
     });
 }
+
+//清除命令
+function clearText(element) {
+    element.value = "";
+}
+
+//绑定匿名函数调用 clearText(element)
+document.getElementById("qr-clear-btn").addEventListener("click", function () {
+    clearText(document.getElementById("qr-input-box"));
+});
+
+
+//QR code 模块
+let options = {
+    text: "qr code"
+};
+let qrcode = new QRCode(document.getElementById("qr-code"), options);
+
+function QRGenerate() {
+    let text = document.getElementById("qr-input-box").value;
+    qrcode.makeCode(text);
+}
+
+//生成QRCode点击按钮绑定
+document.getElementById("qr-btn").addEventListener("click", QRGenerate);
+
+//URL 模块
